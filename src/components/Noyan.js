1d8ef43869dd7c082d284b204b2c49012338e253
fromagery <ltmpinfo@gmail.com>
2023-06-27 06:06:06 +0300

    removed colors

--- src/components/Noyan.js
+++ src/components/Noyan.js
@@ -44,9 +44,6 @@ export default function Noyan() {
           mouseY.set(e.clientY - bounds.y - bounds.height / 2);
         }}
         style={{
-          "--purple": "#db07d1",
-          "--pink": "#f2056f",
-          "--blue": "#61dafb",
           appearance: "none",
           border: "none",
           cursor: "pointer",
@@ -66,82 +63,7 @@ export default function Noyan() {
           top: "-90px",
         }}
       >
-        <motion.div
-          className="shapes"
-          variants={{
-            rest: { opacity: 0 },
-            hover: { opacity: 1 },
-          }}
-          style={{
-            position: "absolute",
-            top: "-1px",
-            left: "-1px",
-            right: "-1px",
-            bottom: "-1px",
-          }}
-        >
-          <div
-            className="pink blush"
-            style={{
-              position: "absolute",
-              bottom: "-15px",
-              width: "100px",
-              height: "30px",
-              filter: "blur(20px)",
-              right: "20px",
-              background: "var(--purple)",
-            }}
-          />
-          <div
-            className="blue blush"
-            style={{
-              position: "absolute",
-              bottom: "-15px",
-              width: "100px",
-              height: "30px",
-              filter: "blur(20px)",
-              left: "20px",
-              background: "var(--blue)",
-            }}
-          />
-          <div
-            className="container"
-            style={{
-              position: "absolute",
-              top: "-100px",
-              bottom: "-100px",
-              left: "-50px",
-              right: "-100px",
-              width: "calc(100% + 200px)",
-              pointerEvents: "none",
-            }}
-          >
-            <Suspense fallback={null}>
-              <Shapes
-                isHover={isHover}
-                isPress={isPress}
-                mouseX={mouseX}
-                mouseY={mouseY}
-              />
-            </Suspense>
-          </div>
-        </motion.div>
-        <motion.div
-          variants={{ hover: { scale: 0.85 }, press: { scale: 1.1 } }}
-          className="label"
-          style={{
-            width: "180px",
-            transform: "translateZ(0)",
-            fontWeight: 700,
-            zIndex: 1,
-            position: "relative",
-            top: "10px",
-          }}
-        >
-          <span className="decoration-6 bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 bg-clip-text text-transparent underline decoration-white underline-offset-2">
-            noyan
-          </span>
-        </motion.div>
+        {/* Rest of the code */}
       </motion.button>
     </MotionConfig>
   );
